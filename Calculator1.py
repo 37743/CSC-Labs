ba9b753b004beda8313f1c57488e1a07d2d228c1
37743 <yousef.ibrahim.gomaa@gmail.com>
2022-06-01 14:46:19 +0200

    Code finished

--- Calculator1.py
+++ Calculator1.py
@@ -13,18 +13,3 @@ class Calculator():
     def div(self, x,y):
         ret = x / y
         return ret
-
-import unittest
-
-class TestProblem1(unittest. TestCase):
-    def test_add(self):
-        self.assertEqual(12,Calculator().add(5,7))
-    def test_sub(self):
-        self.assertEqual(1,Calculator().sub(5,4))
-    def test_mul(self):
-        self.assertEqual(25,Calculator().mul(5,5))
-    def test_div(self):
-        self.assertEqual(3.0,Calculator().div(12,4))
-
-if __name__ == "__main__":
-    unittest.main()
